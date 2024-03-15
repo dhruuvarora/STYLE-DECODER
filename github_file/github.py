049350b139c10eba097c05af0f76bdf4fbf396f7
Dhruv Arora <121032965+dhruuvarora@users.noreply.github.com>
2024-03-15 15:17:48 +0530

    Update github.py

--- github_file/github.py
+++ github_file/github.py
@@ -20,3 +20,38 @@ model.summary()
 model= tf.keras.Sequential([model, GlobalMaxPooling2D()])
 # #model.add(MaxPooling2D)
 model.summary()
+
+import cv2
+import numpy as np
+from numpy.linalg import norm
+
+img=cv2.imread('dataset-card.jpg',0)
+
+cv2.namedWindow("Frame")
+img=cv2.imread("C:\One Drive Data\Desktop\dataset-card.jpg")
+cv2.imshow("Frame", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img=cv2.resize(img,(224,224))
+
+img=np.array(img)
+
+img.shape
+
+# (number_of_image , 224,224,3)
+expand_img=np.expand_dims(img,axis=0)
+
+expand_img.shape
+
+pre_image=preprocess_input(expand_img)
+
+pre_image.shape
+
+result=model.predict(pre_image).flatten()
+
+result.shape
+
+normalized=result/norm(result)
+
+normalized.shape
